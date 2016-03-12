5a1782683a297f033a0ed1b5234b2f31ac4d3491
Sean Monahan <sean@seanmonahan.org>
2016-03-11 19:22:25 -0800

    Add tests for integer and float addition.

---
+++ test/scratchpad.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let calc = require('../src/calc');
+
+console.log('10 + 1:  ' + calc('10 + 1'));
+console.log('10 + 10: ' + calc('10 + 10'));
+console.log('10293292380 + 14560: ' + calc('10293292380 + 14560'));
