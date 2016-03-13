edc1a3235f486b26c05504984dbe10f84a1259a8
Sean Monahan <sean@seanmonahan.org>
2016-03-13 16:33:17 -0700

    Add multiplication tests for floats.

--- test/calc.compute-multiplication.spec.js
+++ test/calc.compute-multiplication.spec.js
@@ -3,7 +3,7 @@
 let expect = require('chai').expect;
 let compute   = require('../src/calc').compute;
 
-describe('compute.js addition', () => {
+describe('compute.js multiplication', () => {
 
     context('integers', () => {
 
@@ -76,69 +76,69 @@ describe('compute.js addition', () => {
         });
     });
 
-    // context('floats', () => {
+    context('floats', () => {
 
-    //     it('should add "1.0 + 1.0"', () => {
-    //         expect(compute('1.0 + 1.0')).to.be.closeTo(2.0, 0.01);
-    //     });
+        it('should add "1.0 * 1.0"', () => {
+            expect(compute('1.0 * 1.0')).to.be.closeTo(1.0, 0.01);
+        });
 
-    //     it('should add "1.1 + 1.0"', () => {
-    //         expect(compute('1.1 + 1.0')).to.be.closeTo(2.1, 0.01);
-    //     });
+        it('should add "1.1 * 1.0"', () => {
+            expect(compute('1.1 * 1.0')).to.be.closeTo(1.1, 0.01);
+        });
 
-    //     it('should add "1.1+1.0"', () => {
-    //         expect(compute('1.1+1.0')).to.be.closeTo(2.1, 0.01);
-    //     });
+        it('should add "1.1*1.0"', () => {
+            expect(compute('1.1*1.0')).to.be.closeTo(1.1, 0.01);
+        });
 
-    //     it('should add "20.123 + 1.2"', () => {
-    //         expect(compute('20.123 + 1.2')).to.be.closeTo(21.323, 0.0001);
-    //     });
+        it('should add "20.123 * 1.2"', () => {
+            expect(compute('20.123 * 1.2')).to.be.closeTo(24.1476, 0.0001);
+        });
 
-    //     it('should add "11.111 + -12.32"', () => {
-    //         expect(compute('11.111 + -12.32')).to.be.closeTo(-1.209, 0.0001);
-    //     });
+        it('should add "11.111 * -12.32"', () => {
+            expect(compute('11.111 * -12.32')).to.be.closeTo(-136.88752, 0.0001);
+        });
 
-    //     it('should add "1.0 + -1.0"', () => {
-    //         expect(compute('1.0 + -1.0')).to.be.closeTo(0.0, 0.01);
-    //     });
+        it('should add "1.0 * -1.0"', () => {
+            expect(compute('1.0 * -1.0')).to.be.closeTo(-1.0, 0.01);
+        });
 
-    //     it('should add "1.1 + -(2.2 + 2.456)"', () => {
-    //         expect(compute('1.1 + -(2.2 + 2.456)')).to.be.closeTo(-3.556, 0.0001);
-    //     });
+        it('should add "1.1 * -(2.2 * 2.456)"', () => {
+            expect(compute('1.1 * -(2.2 * 2.456)')).to.be.closeTo(-5.94352, 0.0001);
+        });
 
-    //     it('should add "-4.211 + -3.456"', () => {
-    //         expect(compute('-4.211 + -3.455')).to.be.closeTo(-7.666, 0.0001);
-    //     });
+        it('should add "-4.211 * -3.456"', () => {
+            expect(compute('-4.211 * -3.455')).to.be.closeTo(14.549005, 0.0001);
+        });
 
-    //     it('should add "102.43123 + 1.00"', () => {
-    //         expect(compute('102.43123 + 1.00')).to.be.closeTo(103.43123, 0.000001);
-    //     });
+        it('should add "102.43123 * 1.00"', () => {
+            expect(compute('102.43123 * 1.00')).to.be.closeTo(102.43123, 0.000001);
+        });
 
-    //     it('should add "1.00 + 102.43123"', () => {
-    //         expect(compute('1.00 + 102.43123')).to.be.closeTo(103.43123, 0.000001);
-    //     });
+        it('should add "1.00 * 102.43123"', () => {
+            expect(compute('1.00 * 102.43123')).to.be.closeTo(102.43123, 0.000001);
+        });
 
-    //     it('should add "-102.43123 + 1.00"', () => {
-    //         expect(compute('-102.43123 + 1.00')).to.be.closeTo(-101.43123, 0.000001);
-    //     });
+        it('should add "-102.43123 * 1.00"', () => {
+            expect(compute('-102.43123 * 1.00')).to.be.closeTo(-102.43123, 0.000001);
+        });
 
-    //     it('should add "1.00 + -102.43123"', () => {
-    //         expect(compute('1.00 + -102.43123')).to.be.closeTo(-101.43123, 0.000001);
-    //     });
+        it('should add "1.00 * -102.43123"', () => {
+            expect(compute('1.00 * -102.43123')).to.be.closeTo(-102.43123, 0.000001);
+        });
 
-    //     it('should add "1.1 + 2.3 + 5.234 + 5.567"', () => {
-    //         expect(compute('1.1 + 2.3 + 5.234 + 5.567')).to.be.closeTo(14.201, 0.0001);
-    //     });
+        it('should add "1.1 * 2.3 * 5.234 * 5.567"', () => {
+            expect(compute('1.1 * 2.3 * 5.234 * 5.567')).to.be.closeTo(73.71832534, 0.0001);
+        });
 
-    //     it('should add "5.567 + 1.1 + 5.234 + 2.3"', () => {
-    //         expect(compute('5.567 + 1.1 + 5.234 + 2.3')).to.be.closeTo(14.201, 0.0001);
-    //     });
+        it('should add "5.567 * 1.1 * 5.234 * 2.3"', () => {
+            expect(compute('5.567 * 1.1 * 5.234 * 2.3')).to.be.closeTo(73.71832534, 0.0001);
+        });
 
-    //     it('should add "89.234 + 1999999.123 + -823472.234234"', () => {
-    //         expect(compute('89.234 + 1999999.123 + -823472.234234')).to.be.closeTo(1176616.122766, 0.0000001);
-    //     });
+        it('should add "89.234 * 1999999.123 * -823472.234234"', () => {
+            expect(compute('89.234 * 1999999.123 * -823472.234234')).to.be.closeTo(-146963378255803.888368564988, 0.0000000000001);
+        });
 
-    // });
+    });
 
     // context('integers + floats', () => {
 
